cf9337240bf0424217616690f943d7510e60511d
Jean <83733853+Jean-Delfino@users.noreply.github.com>
2024-01-17 20:11:34 -0300

    Fixed singleton in Spawner

--- Assets/Reuse/Pooling/Spawner.cs
+++ Assets/Reuse/Pooling/Spawner.cs
@@ -1,37 +1,29 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Reuse.Patterns;
 using UnityEngine;
 
 namespace Reuse.Pooling
 {
-    public class Spawner : MonoBehaviour
+    public class Spawner : Singleton<Spawner>
     {
-        public List<PooledObjects> initialPooledObjects;
-
-        private static Spawner _instance;
-
-        private readonly Dictionary<string, Queue<GameObject>> _pool = new Dictionary<string, Queue<GameObject>>();
-        
+        [SerializeField] private List<GameObject> initialPool;
+        private readonly Dictionary<string, Queue<GameObject>> _pool = new();
+            
         [Serializable]
         public class PooledObjects
         {
             public int amount;
             public GameObject gameObject;
         }
-
-        private void Awake()
+    
+        protected override void Awake()
         {
-            if (_instance != null)
-            {
-                Destroy(this);
-                return;
-            }
-
-            _instance = this;
-            SendToPool(_instance.initialPooledObjects);
+            base.Awake();
+            SendToPool(initialPool);
         }
-        
+    
         public static void SendToPool(List<PooledObjects> toPool)
         { 
             foreach (var pooled in toPool)
@@ -39,19 +31,28 @@ namespace Reuse.Pooling
                 var pool = GetPool(pooled.gameObject);
                 for (int i = 0; i < pooled.amount; i++)
                 {
-                    var go = Instantiate(pooled.gameObject, _instance.transform);
+                    var go = Instantiate(pooled.gameObject, Instance.transform);
                     go.SetActive(false);
-                    
+                        
                     pool.Enqueue(go);
                 }
             }
         }
-
+    
+        public static void SendToPool(List<GameObject> alreadyInstantiated)
+        {
+            foreach (var go in alreadyInstantiated)
+            {
+                go.SetActive(false);
+                GetPool(go.gameObject).Enqueue(go);
+            }
+        }
+    
         public static GameObject Spawn(GameObject gameObject)
         {
             var pooled = GetPool(gameObject);
             GameObject res;
-            
+                
             if (pooled.Count == 0)
             {
                 res = Instantiate(gameObject);
@@ -61,71 +62,71 @@ namespace Reuse.Pooling
                 res = pooled.Dequeue();
                 res.SetActive(true);
             }
-            
+                
             res.name = gameObject.name;
             return res;
         }
-
+    
         public static GameObject Spawn(GameObject gameObject, Transform parent)
         {
             var go = Spawn(gameObject);
             go.transform.SetParent(parent);
-
+    
             return go;
         }
-
+    
         public static GameObject Spawn(GameObject gameObject, Vector3 position)
         {
             var go = Spawn(gameObject);
             go.transform.position = position;
-
+    
             return go;
         }
-        
+            
         public static GameObject Spawn(GameObject gameObject, Vector3 position, Quaternion rotation)
         {
             var go = Spawn(gameObject, position);
             go.transform.rotation = rotation;
-
+    
             return go;
         }
-
+    
         public static GameObject Spawn(GameObject gameObject, Vector3 position, Quaternion rotation, Transform parent)
         {
             var go = Spawn(gameObject, position, rotation);
             go.transform.SetParent(parent);
-
+    
             return go;
         }
-
+    
         public static void DeSpawn(GameObject gameObject)
         {
             var pooled = GetPool(gameObject);
-            
+                
             gameObject.SetActive(false);
-            gameObject.transform.SetParent(_instance.transform);
-            
+            gameObject.transform.SetParent(Instance.transform);
+                
             pooled.Enqueue(gameObject);
         }
-        
+            
         public static void DeSpawn(GameObject gameObject, float duration)
         { 
-            _instance.StartCoroutine(WaitAndDeSpawn(gameObject, duration));
+            Instance.StartCoroutine(WaitAndDeSpawn(gameObject, duration));
         }
-
+    
         private static IEnumerator WaitAndDeSpawn(GameObject gameObject, float duration)
         {
             yield return new WaitForSeconds(duration);
             DeSpawn(gameObject);
         }
-
+    
         private static Queue<GameObject> GetPool(GameObject gameObject)
         {
             var key = gameObject.name;
-
-            if (!_instance._pool.ContainsKey(key)) _instance._pool.Add(key, new Queue<GameObject>());
-
-            return _instance._pool[key];
+    
+            if (!Instance._pool.ContainsKey(key)) Instance._pool.Add(key, new Queue<GameObject>());
+    
+            return Instance._pool[key];
         }
     }
 }
